0f1d310271ceca86658a388611ebc9195571a42d
unknown <newsujans@gmail.com>
2024-02-25 21:36:21 +0545

    init and added libraries

---
+++ main.py
@@ -0,0 +1,7 @@
+ 
+from email.mime.text import MIMEText 
+from email.mime.image import MIMEImage 
+from email.mime.application import MIMEApplication 
+from email.mime.multipart import MIMEMultipart 
+import smtplib 
+import os
